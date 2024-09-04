83197f5d6ff7ada3b878cd0903494330bef85e0d
HarshalTe <harshalterekar509@gmail.com>
2024-09-04 15:23:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Npaesfbformat from './Componants/Npaesfbformat';
+function App() {
+  return (
+    <div className="App">
+         <Npaesfbformat/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componants/Npaesfbformat.js
@@ -0,0 +1,195 @@
+const Npaesfbformat = ()=>{
+    return(
+        <>
+             <div className="container">
+                  <div className="row">
+                        <div className="col-md-12" style={{border:'1px solid black'}}>
+                              <b>NPA TECHNICAL REPORT FORMAT - ESFB</b>
+                        </div>
+                  </div>
+                  <div className="row">
+                       <div className="col-md-2 text-start" style={{border:'1px solid black'}}>Date of visit</div>
+                       <div className="col-md-4" style={{border:'1px solid black'}}></div>
+                       <div className="col-md-2 text-start" style={{border:'1px solid black'}}>Visit Done By</div>
+                       <div className="col-md-4" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                      <div className="col-md-2  text-start " style={{border:'1px solid black'}}>
+                         Date of Valuation Report
+                      </div>
+                      <div className="col-md-10" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                      <div className="col-md-2  text-start " style={{border:'1px solid black'}}>
+                          Application No
+                      </div>
+                      <div className="col-md-10" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                      <div className="col-md-2  text-start " style={{border:'1px solid black'}}>
+                         Name of the Applicant
+                      </div>
+                      <div className="col-md-10" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                      <div className="col-md-2  text-start " style={{border:'1px solid black'}}>
+                         Property Address
+                      </div>
+                      <div className="col-md-10" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                      <div className="col-md-2  text-start " style={{border:'1px solid black'}}>
+                         Nearest Land Mark
+                      </div>
+                      <div className="col-md-10" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                      <div className="col-md-2  text-start " style={{border:'1px solid black'}}>
+                         Date of Valuation Report
+                      </div>
+                      <div className="col-md-10" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                      <div className="col-md-2  text-start " style={{border:'1px solid black'}}>
+                      Latitude and Longitude
+                      </div>
+                      <div className="col-md-10" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                      <div className="col-md-2  text-start " style={{border:'1px solid black'}}>
+                      Age of property
+                      </div>
+                      <div className="col-md-10" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                      <div className="col-md-2  text-start " style={{border:'1px solid black'}}>
+                       Property Usage
+                      </div>
+                      <div className="col-md-10" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                      <div className="col-md-2  text-start " style={{border:'1px solid black'}}>
+                      Whether property is 
+                      identifiable
+                      </div>
+                      <div className="col-md-10" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                      <div className="col-md-2  text-start " style={{border:'1px solid black'}}>
+                         Occupancy Status
+                      </div>
+                      <div className="col-md-10" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                      <div className="col-md-2  text-start " style={{border:'1px solid black'}}>
+                           Person Met at site
+                      </div>
+                      <div className="col-md-10" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                        <div className="col-md-12" style={{border:'1px solid black'}}>
+                              <b>Boundaries</b>
+                        </div>
+                  </div>
+                  <div className="row">
+                       <div className="col-md-2 text-start" style={{border:'1px solid black'}}>Description</div>
+                       <div className="col-md-2" style={{border:'1px solid black'}}>North</div>
+                       <div className="col-md-2" style={{border:'1px solid black'}}>South</div>
+                       <div className="col-md-2" style={{border:'1px solid black'}}>East</div>
+                       <div className="col-md-2" style={{border:'1px solid black'}}>West</div>
+                       <div className="col-md-2" style={{border:'1px solid black'}}>Remarks</div>
+                  </div>
+                  <div className="row">
+                       <div className="col-md-2 text-start" style={{border:'1px solid black'}}>Boundary as per Agreement</div>
+                       <div className="col-md-2" style={{border:'1px solid black'}}></div>
+                       <div className="col-md-2" style={{border:'1px solid black'}}></div>
+                       <div className="col-md-2" style={{border:'1px solid black'}}></div>
+                       <div className="col-md-2" style={{border:'1px solid black'}}></div>
+                       <div className="col-md-2" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                       <div className="col-md-2 text-start" style={{border:'1px solid black'}}>Boundary as per site investigation</div>
+                       <div className="col-md-2" style={{border:'1px solid black'}}></div>
+                       <div className="col-md-2" style={{border:'1px solid black'}}></div>
+                       <div className="col-md-2" style={{border:'1px solid black'}}></div>
+                       <div className="col-md-2" style={{border:'1px solid black'}}></div>
+                       <div className="col-md-2" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                        <div className="col-md-12" style={{border:'1px solid black'}}>
+                              <b>Valuation</b>
+                        </div>
+                  </div>
+                  <div className="row">
+                       <div className="col-md-2 text-start" style={{border:'1px solid black'}}>Plot Area</div>
+                       <div className="col-md-3 justify-content-enter align-item-center" style={{border:'1px solid black'}}></div>
+                       <div className="col-md-3 justify-content-enter align-item-center" style={{border:'1px solid black'}}></div>
+                       <div className="col-md-4 justify-content-enter align-item-center" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                       <div className="col-md-2 text-start" style={{border:'1px solid black'}}>Ground floor First floor</div>
+                       <div className="col-md-3 justify-content-enter align-item-center" style={{border:'1px solid black'}}></div>
+                       <div className="col-md-3 justify-content-enter align-item-center" style={{border:'1px solid black'}}></div>
+                       <div className="col-md-4 justify-content-enter align-item-center" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                       <div className="col-md-2 text-start" style={{border:'1px solid black'}}>Second floor & Above</div>
+                       <div className="col-md-3 justify-content-enter align-item-center" style={{border:'1px solid black'}}></div>
+                       <div className="col-md-3 justify-content-enter align-item-center" style={{border:'1px solid black'}}></div>
+                       <div className="col-md-4 justify-content-enter align-item-center" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                      <div className="col-md-2  text-start " style={{border:'1px solid black'}}>
+                       SUBA / BUA 
+                      (Considered for Valuation)
+                      </div>
+                      <div className="col-md-10" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                      <div className="col-md-2  text-start " style={{border:'1px solid black'}}>
+                      Final Market Value 
+                      (INR)
+                      </div>
+                      <div className="col-md-10" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                      <div className="col-md-2  text-start " style={{border:'1px solid black'}}>
+                        Forced Sale Value(INR)
+                      </div>
+                      <div className="col-md-10" style={{border:'1px solid black'}}></div>
+                  </div>
+                  <div className="row">
+                        <div className="col-md-12" style={{border:'1px solid black'}}>
+                              <b>REMARKS ON THE PROPERTY</b>
+                        </div>
+                  </div> 
+                  <div className="row">
+                      <div className="col-md text-start" style={{border:'2px solid black'}}>
+                         <ol style={{ margin: 0, padding: 0, listStylePosition: 'inside' }}>
+                           <li>The property under consideration is Residential House of Ground Floor storied residential load bearing structure without lift.</li>
+                           <li>As per site observation access to the building is as per the road width is 10 feet approx.</li>
+                           <li>At the time of visit we have observed that property was self occupied since last 25 years.</li>
+                           <li>Kindly check the CC OC & Sanction Plan at your end before disbursement.</li>
+                           <li>Four Boundaries Letter in the name of Mrs. Salekar Hirabai Vasant are made available for verification.</li>
+                           <li>Simple Mortgage Deed made between Mrs. Hirabai Vasant Salekar & Mr. Pramod Vasant Salekar & Mrs. Sima Pramod Salekar AND Equitas Small Finance Bank Limited on dated 29/7/2021 are made available for verification (3731/2021/Khalapur).</li>
+                           <li>The said building is located in Gaothan Area. No technical documents are made available for verification also note that DP plan not made available for verification hence kindly check the same at your end before disbursement.</li>
+                           <li>Since there is no clarity on Land Area as per the provided documents hence we have considered 20% Loading on measured carpet area to derive BUA to furnish the report.</li>
+                           <li>Kindly note that the captioned property is having poor marketability in open market hence kindly check your policy before funding such types of properties.</li>
+                           <li>As per our observation there is a lack of social infrastructure in the nearby vicinity of subject project so marketability will be below average which is to be noted.</li>
+                        </ol>
+                      </div>
+                  </div>
+                  <div className="row">
+                      <div className="col-md-2 text-start" style={{border:'2px solid black'}}>
+                      Authorized Signatory
+                      </div>
+                       <div className="col-md-10" style={{border:'2px solid black'}}></div>
+                  </div>
+
+             </div>
+             
+        
+        </>
+    );
+}
+export default Npaesfbformat;
